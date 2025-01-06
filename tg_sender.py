d1dbcadc2875a06a73c34aa70fa8a04da01e4d45
Artem <bannderovaa@gmail.com>
2025-01-06 10:41:30 +0600

    отлаодочная строка для tg-sender

--- tg_sender.py
+++ tg_sender.py
@@ -3,6 +3,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 def send_to_telegram(msg, tg_api_key, tg_chat_id):
+    logger.debug(f"Вызвана процедура: send_to_telegram(msg={msg}, tg_api_key={tg_api_key}, tg_chat_id={tg_chat_id})")
     if len(tg_api_key) > 0 and len(tg_chat_id) > 0:
         url = f"https://api.telegram.org/bot{tg_api_key}/sendMessage"
         data = {"chat_id": tg_chat_id, "text": msg}
